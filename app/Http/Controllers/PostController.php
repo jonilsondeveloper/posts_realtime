91a9e538b33164353703026c3bb8bcbe805be7fd
jonilsondeveloper <jonilsondeveloper@gmail.com>
2019-05-04 20:33:32 -0300

    commit inicial

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function __construct(User $user, Post $post) {
+        $this->middleware('auth');
+        $this->user = $user;
+        $this->post = $post;
+    }
+
+    public function index() {
+        $posts = $this->post->orderBy('data_time', 'desc')->get();
+
+        return view('posts', compact('posts'));
+    }
+
+    public function store(Request $request) {
+        date_default_timezone_set('America/Fortaleza');
+        $date_time = date('Y-m-d H:i:s');
+
+        $post = $this->post->create([
+            'text' => $request->new_post,
+            'data_time' => $date_time,
+            'user_id' => auth()->user()->id
+        ]);
+
+        if ($post)
+            return redirect()
+                        ->route('home')
+                        ->with('success', 'Sucesso!');
+
+        return redirect()
+                    ->back()
+                    ->withInput()
+                    ->with('error', 'Ocorreu um erro, tente em instantes!');
+    }
+
+    public function show($id) {
+        //
+    }
+}
+
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Postagens em Tempo Real</div>
+
+                <div class="card-body">
+                    @if(session('success'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('success') }}
+                        </div>
+                        <hr>
+                    @endif
+
+                    @if(session('error'))
+                        <div class="alert alert-danger" role="alert">
+                            {{ session('error') }}
+                        </div>
+                        <hr>
+                    @endif
+
+                    <form action="{{ route('posts.store') }}" method="post" id="form_new_post">
+                        @csrf
+                        <div class="form-group">
+                            <label for="new_post">Nova Mensagem</label>
+                            <textarea class="form-control" name="new_post" id="new_post" cols="83" rows="4" value="{{ old('new_message') }}" required></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-success">Enviar Posts</button>
+                    </form>
+
+
+                    <br>
+                    <hr>
+                    <br>
+
+                    @if (count($posts) > 0)
+                        <ul class="list-group" id="posts_list">
+                            @foreach ($posts as $post)
+                                <li class="list-group-item">
+                                    <p class="font-weight-bold">{{ $post->text }}</p>
+                                    <p class="font-italic">
+                                        Autor: {{ $post->user->name }}
+                                    </p>
+                                    <p class="font-italic">
+                                        Data e Hora: {{ date("d/m/Y - H:i:s", strtotime($post->data_time)) }}
+                                    </p>
+                                </li>
+                            @endforeach
+                        </ul>
+                    @else
+                        Nenhuma mensagem salva!
+                    @endif
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
